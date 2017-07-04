d79ed5950b10fcf9f1a16501a40204f4a1fa90cf
Bernd Porr <mail@berndporr.me.uk>
2017-07-04 11:43:21 +0100

    Merge messed it up. Fix it.

--- neuron.cpp
+++ neuron.cpp
@@ -209,17 +209,6 @@ void Neuron::initWeights(double _max, int initBias) {
 }
 
 double Neuron::getAvgWeight(int _input) {
-	double w = 0;
-	for(int j=0;j<nFilters;j++) {
-		w += weights[_input][j];
-	}
-	w += biasweight;
-	return w;
-}
-
-
-
-double Neuron::getAvgWeight(int _input) {
 	int n=0;
 	double w=0;
 	if (_input < 0) {
