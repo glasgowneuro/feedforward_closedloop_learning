5debae108f2722d294808f912fd19fa584c637e4
Bernd Porr <mail@berndporr.me.uk>
2017-09-22 18:58:27 +0100

    Moved the RANGE_CHECK define into the Makefile so that it's in one place. Comment / uncomment there for debug / production.

--- globals.h
+++ globals.h
@@ -1,14 +1,10 @@
 #ifndef GLOBALS_H
 #define GLOBALS_H
 
-// if enabled then range checks are made
-// and assert() is executed
-
-#define RANGE_CHECKS
-
-
-
 // This disables the assert statement
+// The define RANGE_CHECKS is defined in the Makefile
+// for debugging purposes together with the
+// optimisation flags
 
 #ifndef RANGE_CHECKS
 #define NDEBUG
