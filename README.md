cef7d7fd3659c2e0916b620fd4bddaccafde2a42
Bernd Porr <mail@berndporr.me.uk>
2018-11-18 20:13:55 +0000

    Bumped up the version and added comments to the .h file.

--- README.md
+++ README.md
@@ -1,27 +1,19 @@
-# Feedback closed-loop learning library
-
+# Feedback closed loop learning library
 
 ## Prerequisites
-
 
 Ubuntu xenial or bionic LTS with swig, vizdoom and opencv installed.
 
 
-
 ## How to compile / install?
-
+```
       cmake .
       make
       sudo make install
-      sudo ./setup.py install (or ./setup.py --user for local install)
-
+      sudo ./setup.py install
+```
 
 ## Tests / demos
 
-These are in tests_c and tests_py. The linefollower directory contains
-a simple linefollower. See the README files in the subdirectories.
-
-
-## Vizdoom
-
-Check out the README in the vizdoom directory
+These are in tests_c and tests_py to demonstrate the C API and the python
+API.
--- fcl.h
+++ fcl.h
@@ -24,19 +24,33 @@
 class FeedbackClosedloopLearning {
 
 public:
-	// deep fbl without any filters
+	
+	/** 
+	 * Constructor: FCL without any filters
+	 * @num_of_inputs: number of inputs in the input layer
+	 * @num_of_hidden_neurons_per_layer_array: number of neurons in each layer
+	 * @_num_hid_layers: number of hidden layer (needs to match with array above)
+	 * @num_outputs: number of output in the output layer
+	 **/
 	FeedbackClosedloopLearning(
 			int num_of_inputs,
 			int* num_of_hidden_neurons_per_layer_array,
 			int _num_hid_layers,
 			int num_outputs);
 
-	// deep fbl with filters for both the input and hidden layer
-	// filter number >0 means: filterbank
-	// filter number = 0 means layer without filters
-	// filter parameters: are in time steps. For ex, minT = 10 means
-	// a response of 10 time steps for the first filter and that goes
-	// up to maxT time steps, for example maxT = 100 or so.
+	/**
+	 * Constructor: FCL with filters for both the input and hidden layers
+	 * filter number >0 means: filterbank
+	 * filter number = 0 means layer without filters
+	 * @num_of_inputs: number of inputs in the input layer
+         * @num_of_hidden_neurons_per_layer_array: number of neurons in each layer
+	 * @_num_hid_layers: number of hidden layer (needs to match with array above)
+	 * @num_outputs: number of output in the output layer
+         * @num_filtersInput: number of filters at the input layer
+         * @num_filtersHidden: number of filters in the hiddel layers (usually zero)
+         * @_minT: minimum/first temporal duration of the 1st filter
+         * @_maxT: maximum/last temporal duration of the last filter
+         **/
 	FeedbackClosedloopLearning(
 			int num_of_inputs,
 			int* num_of_hidden_neurons_per_layer_array,
@@ -47,53 +61,121 @@ public:
 			double _minT,
 			double _maxT);
 
-	// destructor
+	/**
+         * Destructor
+         **/
 	~FeedbackClosedloopLearning();
 
-	// here is where all the magic is happening
+	/**
+         * Performs the simulation step
+         * @input: Array with the input values
+         * @error: Array of the error signals
+         **/
 	void doStep(double* input, double* error);
 
-	// here is where all the magic is happening with array range checks
+	// For the python wrapper
 	void doStep(double* input, int n1, double* error, int n2);
 
-	// get the output of the network
+	/**
+         * Gets the output from one of the output neurons
+         **/
 	double getOutput(int index) {
 		return layers[num_hid_layers]->getOutput(index);
 	}
 
-	// set globally the learning rate
+	/**
+         * Sets globally the learning rate
+         * @learningRate for all layers and neurons.
+         **/
 	void setLearningRate(double learningRate);
 
-	// sets how the learnign rate increases or decreases in deeper layers
+	/**
+         * Sets how the learnign rate increases or decreases from layer to layer
+         * @_learningRateDiscountFactor: >1 means higher learning rate in deeper layers
+         **/
 	void setLearningRateDiscountFactor(double _learningRateDiscountFactor) {
 		learningRateDiscountFactor = _learningRateDiscountFactor;
 	}
 
-	// global momentum for all layers
+	/**
+         * Sets a typical weight decay scaled with the learning rate
+         * @decay: >0, the larger the faster the decay
+         **/
+	void setDecay(double decay);
+
+	/**
+	 * Sets the global momentum for all layers
+         **/
 	void setMomentum(double momentum);
 
+	/**
+         * Sets the activation function of the Neuron
+         * @_activationFunction: see Neuron::ActivationFunction for the different options
+         **/
 	void setActivationFunction(Neuron::ActivationFunction _activationFunction);
 
+	/**
+         * Inits the weights in all layers
+         * @max: Maximum value of the weights
+         * @initBias: If the bias also should be initialised
+         * @weightInitMethod: see Neuron::WeightInitMethod for the options
+         **/
 	void initWeights(double max = 0.001,
 			 int initBias = 1,
 			 Neuron::WeightInitMethod weightInitMethod = Neuron::MAX_OUTPUT_RANDOM);
 
+	/**
+         * Seeds the random number generator
+         **/
 	void seedRandom(int s) { srand(s); };
 
+	/**
+         * Sets globally the bias
+         **/
 	void setBias(double _bias);
 
-	void setDecay(double decay);
-	
+	/**
+         * Returns the number of hidden layers
+         **/
 	int getNumHidLayers() {return num_hid_layers;};
+
+	/**
+         * Gets the total number of layers
+         **/
 	int getNumLayers() {return num_hid_layers+1;};
-	
+
+	/**
+         * Gets a pointer to a layer
+         * @i: Index of the layer
+         **/
 	Layer* getLayer(int i) {assert (i<=num_hid_layers); return layers[i];};
+
+	/**
+         * Gets the output layer
+         **/
 	Layer* getOutputLayer() {return layers[num_hid_layers];};
+
+	/**
+         * Returns all Layers
+         **/
 	Layer** getLayers() {return layers;};
 
+	/**
+         * Sets if the learning should be the derivative of the error in each neuron
+         * @useIt: If one the derivative is used.
+         **/
 	void setUseDerivative(int useIt);
 
+	/**
+         * Saves the whole network
+         * @name: filename
+         **/
 	bool saveModel(const char* name);
+
+	/**
+         * Loads the while network
+         * @name: filename
+         **/
 	bool loadModel(const char* name);
 
 	
